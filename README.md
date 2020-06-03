4af5f68028b3863ca4326d1e18e01957a934c6f9
Michael W Lee <mikewclee@gmail.com>
2020-06-02 22:15:36 -0400

    js, css, html updates

--- README.md
+++ README.md
@@ -8,21 +8,18 @@ It will also feature a clean and polished user interface and be responsive, ensu
 
 ## Acceptance Criteria
 
-```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
+Click the start button to take the code quiz
+THEN a timer starts and you be presented with a question
+WHEN you answer a question
+THEN you are presented with another question
+WHEN you answer a question incorrectly
 THEN time is subtracted from the clock
 WHEN all questions are answered or the timer reaches 0
 THEN the game is over
 WHEN the game is over
-THEN I can save my initials and score
+THEN you can save your initials and score.
 ```
 
-
 The following is screenshot of app:
-![code quiz](assets/CodeQuizApp.JPG)
-![code quiz](assets/CodeQuizApp2.JPG)
+![codeQuiz](assets/images/CodeQuizApp.JPG)
+![codeQuiz2](assets/images/CodeQuizApp2.JPG)
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,7 @@
 //Delcare variables
 var score = 0;
 var timeLeft = 75;
-var timer;
-
+var timer=0;
 
 //arrray questions- titles, choices, and answers     
 var questions = [{
@@ -32,11 +31,10 @@ var questions = [{
 }
 ]
 
-//Click start button to start timer and quiz
+//Click start button to start timer and call nextQtn function to start questions
 function startBtn() {
   timeLeft = 75;
   document.getElementById("timeLeft").innerHTML = timeLeft;
-  // console.log(timeLeft);
 
   timer = setInterval(function () {
     timeLeft--;
@@ -53,37 +51,48 @@ function startBtn() {
 
 // set initial questions array index before ++
 var currentQuestion = -1;
-// loops thru questions array 
+// loops thru questions object 
 function nextQtn() {
   currentQuestion++;
   // check to see if all questions been asked
   if (currentQuestion === questions.length) {
+    document.getElementById("answerResponse").textContent = 'Great Job!' ;
     endGame();
     return;
   }
   // var to hold arr questions.title 
   var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
-
-  // loop through questions choices 
+  // loop through arr questions.choices 
   for (var i = 0; i < questions[currentQuestion].choices.length; i++) {
-    
-    var buttonCode = "<button onclick=questions[currentQuestion].choice[i]";
-    var answerCode = questions[currentQuestion].answer;
+    // setup buttons with arr questions.choices
+    var buttonCode = "<button onclick=\"[ANSWER]\">[CHOICE]</button>";
+    buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[i]);
     // console.log('buttoncode is ' + buttonCode);
-    // console.log('answercode is ' + answerCode);
-
-      if (buttonCode == answerCode) {
-      buttonCode = "<button onclick=correct()>";
-      console.log('buttoncode is ' + buttonCode);
+    
+    // check if click choice is same as answer in questions arr 
+    if (questions[currentQuestion].choices[i] == questions[currentQuestion].answer) {
+      buttonCode = buttonCode.replace("[ANSWER]", "correct()");
     } else {
-      buttonCode = "<button onclick=incorrect()>";
-      console.log('buttoncode is ' + buttonCode);
+      buttonCode = buttonCode.replace("[ANSWER]", "incorrect()");
     }
-
     quizContent += buttonCode
   }
-
   document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+//deduct 10seconds from the timer if user chooses an incorrect answer
+function incorrect() {
+  timeLeft -= 10;
+  console.log('timeleft incorrect' + timeLeft);
+  document.getElementById("answerResponse").textContent = 'Wrong!';
+  nextQtn();
+}
+
+//increases the score by 20points if the user chooses the correct answer
+function correct() {
+  score += 20;
+  document.getElementById("answerResponse").textContent = 'Correct!';
+  nextQtn();
 }
 
 //end game to output score and name
@@ -95,7 +104,7 @@ function endGame() {
   <h3>You got ` + score / 20 + ` questions correct. </h3>
   <h3>That is a score ` + score + ` out of 100.</h3>
   <input type="text" id="name" placeholder="Your Initials"> 
-  <button onclick="setScore()">Set score!</button><br>
+  <button onclick="setScore()">Save score!</button><br>
   <button onclick="resetGame()">Play Again!</button>
   `;
 
@@ -113,20 +122,15 @@ function getScore() {
   var quizContent = `
   <h2>` + localStorage.getItem("highscoreName") + `'s last saved score is:</h2>
   <h1>` + localStorage.getItem("highscore") + `</h1><br> 
-  
   <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
-  
   `;
-
   document.getElementById("quizBody").innerHTML = quizContent;
 }
-
 
 //clears the score name and value in the local storage if the user selects 'clear score'
 function clearScore() {
   localStorage.setItem("highscore", "");
   localStorage.setItem("highscoreName", "");
-
   resetGame();
 }
 
@@ -148,19 +152,6 @@ function resetGame() {
       Click start to play!   
   </h3>
   <button onclick="startBtn()">Start!</button>`;
-
   document.getElementById("quizBody").innerHTML = quizContent;
-}
-
-//deduct 10seconds from the timer if user chooses an incorrect answer
-function incorrect() {
-  timeLeft -= 10;
-  console.log('timeleft incorrect' + timeLeft);
-  nextQtn();
-}
-
-//increases the score by 20points if the user chooses the correct answer
-function correct() {
-  score += 20;
-  nextQtn();
+  document.getElementById("answerResponse").textContent = ' ' ;
 }
